88a632f0b2f56e45375967d21bfc4b9d13227ea0
FluffyMaguro <uminomaguro@gmail.com>
2020-10-16 13:24:07 +0200

    2.22 Tweaks

--- SCO.py
+++ SCO.py
@@ -39,7 +39,7 @@ from SCOFunctions.SC2Dictionaries import prestige_names, CommanderMastery
 logger = logclass('SCO','INFO')
 logclass.FILE = truePath("Logs.txt")
 
-APPVERSION = 221
+APPVERSION = 222
 SETTING_FILE = truePath('Settings.json')
 
 
